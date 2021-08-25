034d7acbf281f41e8e90cde8db943e5c5d88d999
Mauri <dronM27a.z@gmail.com>
2021-08-25 16:59:11 -0300

    Create BodyMovies.js

---
+++ src/components/BodyMovies/BodyMovies.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Buscador from '../Buscador/Buscador';
+import Movies from '../Movies/Movies';
+
+import style from './BodyMovies.module.css';
+
+export default function BodyMovies() {
+
+const movies = useSelector(state => state.moviesSearch);
+
+if (movies.length === 0) {
+    return (
+        <div className={style.container}>
+            <div className={style.Buscador_componente}>
+                <Buscador />
+            </div>
+            {/* {alert('No se pudo acceder a la base de datos')} */}
+        </div>
+)}
+
+    return (
+        <div className={style.cont_con_movies}>
+            <div className={style.Buscador_componente}>
+                <Buscador />
+            </div>
+            <div className={style.Movies_componente}>
+                <Movies allMovies={movies}/>
+            </div>
+        </div>
+    )
+}
