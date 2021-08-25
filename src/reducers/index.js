7637dd30f91d22c3587a7a4fae767af5e64ee92b
Mauri <dronM27a.z@gmail.com>
2021-08-25 16:47:46 -0300

    Create index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+import { GET_MOVIES, ADD_FAVOURITE_MOVIE, REMOVE_FAVOURITE, GET_DETAIL_MOVIE } from "../actions";
+
+const initialState = {
+    moviesSearch: [],
+    favouritesMovies: [],
+    detailMovie: {}
+}
+
+export function rootReducer(state=initialState, action) {
+    if (action.type === GET_MOVIES){
+        return {
+            ...state,
+            moviesSearch: action.payload.Search
+        }
+    }
+    if (action.type === ADD_FAVOURITE_MOVIE){
+        return {
+            ...state,
+            favouritesMovies: state.favouritesMovies.concat(action.payload)
+        }
+    }
+    if (action.type === REMOVE_FAVOURITE){
+        return {
+            ...state,
+            favouritesMovies: state.favouritesMovies.filter(m => m.id !== action.payload)
+        }
+    }
+    if (action.type === GET_DETAIL_MOVIE){
+        return {
+            ...state,
+            detailMovie: action.payload
+        }
+    }
+    return state;
+}
+
+export default rootReducer;
