8b995e002372a6eb5f9faae94af186f8a716ecc6
Mauri <dronM27a.z@gmail.com>
2021-08-25 12:51:55 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Favorites from "./components/Favorites/Favorites";
+import NavBar from "./components/NavBar/NavBar";
+import { Route } from "react-router-dom";
+import DetailMovie from "./components/DetailMovie/DetailMovie";
+import BodyMovies from "./components/BodyMovies/BodyMovies";
+
+function App() {
+  return (
+      <React.Fragment>
+          <NavBar />
+          <Route exact path ='/' component={BodyMovies} />
+          <Route path="/favs" component={Favorites} />
+          <Route path="/movie/:id" 
+                 render={({match}) => <DetailMovie idMovie={match.params.id} />} />
+      </React.Fragment>
+  );
+}
+
+export default App;
