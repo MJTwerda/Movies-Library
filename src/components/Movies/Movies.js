8d39fda87c6be80126526b10fd70e6572d367c00
Mauri <dronM27a.z@gmail.com>
2021-08-25 16:51:38 -0300

    Create Movies.js

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,31 @@
+import React from 'react'; 
+import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavouriteMovies } from '../../actions/index';
+
+import style from './Movies.module.css';
+
+export default function Movies({allMovies}) {
+
+    const dispatch = useDispatch();
+    
+    return (
+        <div className={style.cuerpoPelis}>
+    
+          {allMovies === undefined ? alert('No se encontraron resultados.. Posiblemente su película es malísima')
+          : allMovies?.map(s => 
+
+          <div className={style.indMovie} key={s.id}>
+            <NavLink to={`movie/${s.imdbID}`} className={style.indLink}>
+          <img src={s.Poster} className={style.imgMovie} alt='No encontrada'/>
+            </NavLink>
+
+              <button className={style.btnFav} onClick={() => 
+              dispatch(addFavouriteMovies({title: s.Title, id: s.imdbID, image: s.Poster}))}>Fav
+              </button>
+          </div> 
+          )} 
+      </div>
+    )
+  }
+
