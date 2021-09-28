d9de80279eb4996e9b413be6fae14599665a5c90
Mauri <dronm27a.z@gmail.com>
2021-09-28 12:32:59 -0300

    change info movie

--- src/components/DetailMovie/DetailMovie.js
+++ src/components/DetailMovie/DetailMovie.js
@@ -14,11 +14,20 @@ export default function DetailMovie({idMovie}) {
     
   return(
     <div className={style.container}>
-      <h4>DETALLES</h4>
-      <img src={detail.Poster} alt='Search...' />
-      <h5>{detail.Runtime}</h5>
-      <h5>{detail.Actors}</h5>
-      <h5>{detail.Plot}</h5>
+      <h3 className={style.title}>Detalles de {detail.Title}</h3>
+      <div className={style.detailContainer}>
+        <img className={style.img} src={detail.Poster} alt='Search...' />
+        <div className={style.text_Detail}>
+          <h4>Duración: {detail.Runtime}</h4>
+          <h4>Géneros: {detail.Genre}</h4>
+          <h4>Elenco: {detail.Actors}</h4>
+          <h4>{detail.Plot}</h4>
+          <h4>Director: {detail.Director}</h4>
+          <h4>Escritores: {detail.Writer}</h4>
+          <h4>Producción: {detail.Production}</h4>
+          <h4>Premios: {detail.Awards}</h4>
+        </div>
+      </div>
     </div>
   )
 } 
